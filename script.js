9dee13b2d8854a6bd4fae21e0e6c552eb5c6797f
Rabchunski Stanislau <stanislav.ryabchinsky@mail.ru>
2023-05-09 18:22:40 +0300

    fixed:logo header, optimized code

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 {
 window.onscroll = function(){stickyHeader()};
 const header = document.getElementById("Header");
-const logo = document.getElementById("headerLogo")
-const scrollLogo = document.getElementById("headerScrollLogo")
+const logo = document.getElementById("headerLogo");
 const sticky = header.offsetTop;
-const headerCh = document.querySelector(".header__menu__name")
+
 function stickyHeader(){
     if(window.pageYOffset >= sticky) {
-        header.classList.add("sticky","backgroundHeader" , "header__nav__scroll" , "header__menu__name");
-        logo.classList.add("header__logo");
-        scrollLogo.classList.add("header__scroll__logo");
-
-        // headerCh.classList.prepend(".header__scroll__logo");
-
-        // headerCh.replaceChild(".header__scroll__logo" , ".header__logo");
+        header.classList.add("sticky","backgroundHeader" , "header__nav__scroll" );
+        logo.src = "./assets/logo/nodyFooter.svg";
     } else {
-        header.classList.remove("sticky" ,"backgroundHeader" , "header__nav__scroll", "header__menu__name");
-        logo.classList.remove("header__logo");
-        scrollLogo.classList.remove("header__scroll__logo");
-
-        // headerCh.classList.prepend(".header__logo");
-
-        // headerCh.replaceChild(".header__logo" , ".header__scroll__logo");
-
+        header.classList.remove("sticky" ,"backgroundHeader" , "header__nav__scroll");
+        logo.src = "./assets/logo/logo.svg";
     }
 }
 }
